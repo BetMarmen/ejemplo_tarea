0260dcede29a6d463e222b1accb1b0b18dd65193
Belen Marquez <110306602+BetMarmen@users.noreply.github.com>
2023-05-04 00:24:00 -0600

    Add files via upload

---
+++ ejemplo/src/ejemplo/main.java
@@ -0,0 +1,17 @@
+package ejemplo;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		persona juan = new persona();
+		System.out.println("Hola te presento a mi tio Juan caldera:");
+		System.out.println("Nombre: "+juan.nombre);
+		System.out.println("Apellido: "+juan.apellido);
+		System.out.println("Edad: "+juan.edad);
+		System.out.println("Profesion : "+juan.profesion);
+		System.out.println("Es casado?: "+juan.casado);
+		
+	}
+
+}
---
+++ ejemplo/src/ejemplo/persona.java
@@ -0,0 +1,22 @@
+package ejemplo;
+
+public class persona {
+	String nacionalidad;
+	String nombre;
+	String apellido;
+	int edad;
+	boolean casado;
+	String profesion;
+	public persona() {
+		nacionalidad= "Nica";
+		nombre="Juan";
+		apellido="Caldera";
+		edad=50;
+		casado= false;
+		profesion = "Cantante";
+		
+		
+				
+	}
+	
+}
